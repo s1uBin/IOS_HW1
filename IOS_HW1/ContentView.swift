15752e8ece2760e94f60398e1c6a7ecfb27d6f4c
HsiehYuHeng <k28998989@gmail.com>
2019-09-24 13:17:42 +0800

    Initial Commit

---
+++ IOS_HW1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOS_HW1
+//
+//  Created by User17 on 2019/9/24.
+//  Copyright © 2019 00557052. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
